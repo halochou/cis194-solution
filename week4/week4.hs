2b7568089cdcf68a5bb7aeebeac54965173aab1f
halochou <halo.chou@gmail.com>
2016-06-13 16:22:59 +0800

    week4 ex2

--- week4/week4.hs
+++ week4/week4.hs
@@ -13,9 +13,6 @@ fun2 :: Integer -> Integer
 fun2 1 = 0
 fun2 n | even n = n + fun2 (n `div` 2)
        | otherwise = fun2 (3 * n + 1)
--- WIP
+-- Ex1
 fun2' :: Integer -> Integer
-fun2' n
-  | even n = sum $ takeWhile (>1) $ iterate (`div` 2) n
-  | otherwise = fun2' (3 * n + 1)
-  
+fun2' = sum . filter (even) . takeWhile (/=1) . iterate (\x -> if even x then x `div` 2 else 3*x+1)
